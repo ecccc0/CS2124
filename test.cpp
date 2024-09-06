149dd4129c59b4530a18bcf4bf02af4a37744af2
ecccc0 <eric.ecccc@gmail.com>
2024-09-06 10:30:43 -0400

    First commit

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = -1;
+    cout << "input an integer value" << endl;
+    cin >> x;
+    cout << "x:" << x << endl;
+    return 0;
+}
