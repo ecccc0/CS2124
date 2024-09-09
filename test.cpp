3433bfa09973e574c61bc76d4b4631dc4933e00f
ecccc0 <eric.ecccc@gmail.com>
2024-09-09 14:17:06 -0400

    Notes

--- test.cpp
+++ test.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main(){
-    int x = -1;
-    cout << "input an integer value" << endl;
-    cin >> x;
-    cout << "x:" << x << endl;
+    char c = -120;
+    cout << c << endl;
     return 0;
 }
