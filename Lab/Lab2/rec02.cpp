fe5cefed11a7ea5a8c44acfcef7307db7d6b15fe
ecccc0 <eric.ecccc@gmail.com>
2024-09-13 11:57:41 -0400

    Finished Lab2

--- Lab/Lab2/rec02.cpp
+++ Lab/Lab2/rec02.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <utility>
 using namespace std;
 struct Hydrocarbon{
     vector<string> names;
@@ -9,20 +10,30 @@ struct Hydrocarbon{
     int H;
 };
 
-void openStream(ifstream ifs);
-void fillVector(vector<Hydrocarbon> &v, ifstream ifs);
-void display(vector<Hydrocarbon> v);
-void insertToVector(string name, int h, int c);
-int findFormula(vector<Hydrocarbon> v, int h, int c);
-void displaySingle(Hydrocarbon hc);
+void openStream(ifstream &ifs);
+void fillVector(vector<Hydrocarbon> &v, ifstream &ifs);
+void display(const vector<Hydrocarbon> &v);
+void insertToVector(vector<Hydrocarbon> &v, const string &name, int c, int h);
+size_t findFormula(const vector<Hydrocarbon> &v, int c, int h);
+void displaySingle(const Hydrocarbon &hc);
+void sort(vector<Hydrocarbon> &v);
 
 int main(){
+    ifstream ifs;
+    openStream(ifs);
+    vector<Hydrocarbon> v;
+    fillVector(v, ifs);
+    cout << "Unsorted: " << endl;
+    display(v);
+    cout << endl << "Sorted: "<< endl;
+    sort(v);
+    display(v);
 
 
     return 0;
 }
 
-void openStream(ifstream ifs){
+void openStream(ifstream &ifs){
     string filename;
     cout << "Enter file name: ";
     do{
@@ -34,30 +45,39 @@ void openStream(ifstream ifs){
     }while(!ifs);
 }
 
-void fillVector(vector<Hydrocarbon> &v, ifstream ifs){
+void fillVector(vector<Hydrocarbon> &v, ifstream &ifs){
     string name;
-    string formula;
+    char c;
     int cur_c, cur_h;
     bool flag;
-    while (ifs >> name >> formula){
-        int cursor = 1;
-        string h, c;
-        while (formula[cursor] != 'H'){
-            c.push_back(formula[cursor]);
-        }
-        cur_c = stoi(c);
-        h = formula.substr(cursor+1);
-        cur_h = stoi(h);
-        
-        if (flag){
-            vector<string> new_names{name};
-            Hydrocarbon hc{new_names, cur_c, cur_h}; 
-            v.push_back(hc);
-        }
+    while (ifs >> name >> c >> cur_c >> c >> cur_h){
+        cout << name << " " << cur_c << " " << " " << cur_h << endl;
+        // int cursor = 1;
+        // string h, c;
+        // while (formula[cursor] != 'H'){
+        //     c.push_back(formula[cursor]);
+        //     cursor++;
+        // }
+        // cur_c = stoi(c);
+        // h = formula.substr(cursor+1);
+        // cur_h = stoi(h);
+        insertToVector(v, name, cur_c, cur_h);
     }
 }
 
-int findFormula(vector<Hydrocarbon> v, int h, int c){
+void insertToVector(vector<Hydrocarbon> &v, const string &name, int c, int h){
+    size_t index = findFormula(v, c, h);
+    if (index == v.size()){
+        vector<string> new_names{name};
+        Hydrocarbon hc{new_names, c, h}; 
+        v.push_back(hc);
+    } 
+    else{
+        v[index].names.push_back(name);
+    }
+}
+
+size_t findFormula(const vector<Hydrocarbon> &v, int c, int h){
     Hydrocarbon x;
     for (size_t i = 0; i < v.size(); ++i){
         x = v[i];
@@ -66,4 +86,29 @@ int findFormula(vector<Hydrocarbon> v, int h, int c){
         }
     }
     return v.size();
+}
+
+void displaySingle(const Hydrocarbon &hc){
+    cout << 'C' << hc.C << 'H' << hc.H << " ";
+    for (size_t i = 0; i < hc.names.size(); ++i){
+        cout << hc.names[i] << " ";
+    }
+}
+
+void display(const vector<Hydrocarbon> &v){
+    for (size_t i = 0; i < v.size(); ++i){
+        displaySingle(v[i]);
+        cout << endl;
+    }
+}
+
+void sort(vector<Hydrocarbon> &v){
+    // Bubble sort
+    for (size_t i = 0; i < v.size(); ++i){
+        for (size_t j = 0; j < v.size() - 1 - i; ++j){
+            if (v[j].C > v[j+1].C || (v[j].C == v[j+1].C && v[j].H > v[j+1].H)){
+                swap(v[j], v[j+1]);
+            }
+        }
+    }
 }
